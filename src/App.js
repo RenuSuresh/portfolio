9946db624a3e8844da50fc9fd4ccf1661522d925
renu ganvir <rganvir500@gmail.com>
2021-01-19 01:27:08 +0530

    about page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
+import About from "./Components/About";
 import Banner from "./Components/Banner";
 import Header from "./Components/Header";
+import { useEffect, useState } from "react";
 function App() {
+  const [biodata, setBioData] = useState({});
+
+  async function getData() {
+    const res = await fetch("./biodata.json")
+      .then((res) => {
+        return res.json();
+      })
+      .then((result) => {
+        setBioData(result);
+      });
+    console.log("biodata>>>>", biodata);
+  }
+  useEffect(() => {
+    getData();
+    return () => {};
+  }, []);
   return (
     <div className="app">
       <Header />
       <Banner />
+      <About data={biodata.main} />
     </div>
   );
 }
